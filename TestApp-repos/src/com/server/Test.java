363844b95d1bba641cf70024e0948995d0327167
administrator <administrator@PrakashU>
2017-09-20 13:22:56 +0530

    Nest update

--- TestApp-repos/src/com/server/Test.java
+++ TestApp-repos/src/com/server/Test.java
@@ -3,7 +3,7 @@ package com.server;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Test.main() is here");
+		System.out.println("This is test here");
 	}
 
 }
